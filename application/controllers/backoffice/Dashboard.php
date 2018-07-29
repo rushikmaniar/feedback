b072b36fd8ce7fdfda9a1c4067b40d3619aefde5
rushik maniar <rushikmaniar107@gmail.com>
2018-07-29 21:06:23 +0530

    dashboard changes

--- application/controllers/backoffice/Dashboard.php
+++ application/controllers/backoffice/Dashboard.php
@@ -15,6 +15,21 @@ class Dashboard extends AdminController
 
     public function index()
     {
+        //total employees
+        $total_employees = $this->CommonModel->getRecord('employee_master')->num_rows();
+
+        //total classes
+        $total_class = $this->CommonModel->getRecord('class_master')->num_rows();
+
+        //total entries
+        $total_entries = $this->CommonModel->getRecord('entry_record')->num_rows();
+
+        //total criterias
+        $total_criterias = $this->CommonModel->getRecord('criteria_master')->num_rows();
+
+        $records = array('total_employees'=>$total_employees,'total_class'=>$total_class,'total_entries'=>$total_entries,'total_criterias'=>$total_criterias);
+
+        $this->pageData['records'] = $records;
         $this->pageTitle = 'Dashboard';
         $this->render("dashboard/index");
     }
