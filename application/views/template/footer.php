0022206a0acf6ef370936da9bba3fd0fb14eff56
Rushik <rushikmaniar107@gmail.com>
2018-08-02 10:07:35 +0530

    slider , footer change

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -4,25 +4,12 @@
     <div class="container my-5">
         <div class="py-5">
             <div class="text-center">
-                <h1>🛠</h1>
-                <h2 class="text-white">Build Something!</h2>
+                
+                <h2 class="text-white">Totally Anonymous!</h2>
                 <p class="text-muted col-lg-8 col-md-10 ml-auto mr-auto">What are you waiting for?
                     <b>
-                        <u>Go ahead and build something!
+                        <u>Step Ahead And Share Your View!
                         </u>
-                    </b> Can't wait to see what you come up with! If you like Shards, make sure you share it with your friends too!</p>
-            </div>
-
-            <div class="share d-table ml-auto mr-auto mt-5">
-                <a href="https://twitter.com/intent/tweet?url=https://designrevision.com/download/shards&text=I%27m+in+love+with+Shards%2C+a+free+Bootstrap+4+UI+kit+by+%40DesignRevision+%23ui+%23design+%23free+%23bootstrap"
-                   class="btn btn-pill btn-primary mr-4" title="Share Shards on Twitter">
-                    <i class="fa fa-twitter mr-1"></i> Share on Twitter</a>
-                <a href="https://www.facebook.com/sharer/sharer.php?u=https://designrevision.com/download/shards" class="btn btn-pill btn-primary mr-4"
-                   title="Share on Facebook">
-                    <i class="fa fa-facebook mr-1"></i> Share on Facebook</a>
-                <a href="https://www.linkedin.com/shareArticle?mini=true&title=Shards+%E2%80%94+A+free+and+modern+Bootstrap+4+UI+kit&url=https://designrevision.com/download/shards"
-                   class="btn btn-pill btn-primary mr-4" title="Share on LinkedIn">
-                    <i class="fa fa-linkedin mr-1"></i> Share on LinkedIn</a>
             </div>
         </div>
     </div>
@@ -30,21 +17,6 @@
 
 <footer class="main-footer py-5">
     <p class="text-muted text-center small p-0 mb-4">&copy; Copyright 2018 — Rushik</p>
-    <div class="social d-table mx-auto">
-        <a class="twitter mx-3 h4 d-inline-block text-secondary" href="https://twitter.com/DesignRevision" target="_blank">
-            <i class="fa fa-twitter"></i>
-            <span class="sr-only">View our Twitter Profile</span>
-        </a>
-        <a class="facebook mx-3 h4 d-inline-block text-secondary" href="https://www.facebook.com/designrevision" target="_blank">
-            <i class="fa fa-facebook"></i>
-            <span class="sr-only">View our Facebook Profile
-            <span>
-        </a>
-        <a class="github mx-3 h4 d-inline-block text-secondary" href="https://github.com/designrevision" target="_blank">
-            <i class="fa fa-github"></i>
-            <span class="sr-only">View our GitHub Profile</span>
-        </a>
-    </div>
 </footer>
 
 <div class="modal fade" id="feedback_frontsite_modal" tabindex="-1" role="dialog" >
